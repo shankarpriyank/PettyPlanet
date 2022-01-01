51be511b0c754b56543095e2149f836d27b21d09
shankarpriyank <shankarpriyank312002@gmail.com>
2022-01-01 13:16:35 +0530

    Firestore Connected

--- app/src/main/java/com/example/pettyplanet/MainActivity.kt
+++ app/src/main/java/com/example/pettyplanet/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import com.example.pettyplanet.daos.UserDao
+import com.example.pettyplanet.models.User
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -18,9 +22,13 @@ const val RC_SIGN_IN = 123
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
 
-        //Animate Gradient
+
+
+         //Animate Gradient
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         val animDrawable = root_layout.background as AnimationDrawable
         animDrawable.setEnterFadeDuration(10)
@@ -39,6 +47,16 @@ class MainActivity : AppCompatActivity() {
             val signInIntent = mGoogleSignInClient.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
     }
 
     private fun signin() {
@@ -62,15 +80,36 @@ class MainActivity : AppCompatActivity() {
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
-            login_tv.text = account.displayName
+
+             updateUI(account)
+//            val mainActivityIntent = Intent(this, FeedActivity::class.java)
+//            startActivity(mainActivityIntent)
+//            finish()
 
         } catch (e: ApiException) {
+            Log.e("Err","$e")
+            Toast.makeText(this,"Login Failed",Toast.LENGTH_LONG).show()
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
 
 
         }
 
+    }
+    private fun updateUI(User: GoogleSignInAccount?) {
+        if(User != null) {
+
+            val user = User(User.id, User.displayName, User.photoUrl.toString())
+            val usersDao = UserDao()
+            usersDao.addUser(user)
+
+            val mainActivityIntent = Intent(this, FeedActivity::class.java)
+            startActivity(mainActivityIntent)
+            finish()
+        } else {
+            return
+
+        }
     }
 
 
---
+++ app/src/main/java/com/example/pettyplanet/models/User.kt
@@ -0,0 +1,6 @@
+package com.example.pettyplanet.models
+
+data class User(
+    val uid: String? = "",
+    val displayName: String? = "",
+    val imageUrl: String? = "")
